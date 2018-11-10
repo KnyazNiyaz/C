02de6169b3cba951c4ae403e38bd64d327fe1318
Emil Niyaz <niyaz.emil@gmail.com>
2018-11-10 18:31:09 -0500

    float, double,long double

--- size-of-types/types-size.c
+++ size-of-types/types-size.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
  
 int main() {
+
+	long float f;
     printf("sizeof(char)  = %d\n", sizeof(char));
     printf("sizeof(short) = %d\n", sizeof(short));
     printf("sizeof(int)   = %d\n", sizeof(int));
     printf("sizeof(long)  = %d\n", sizeof(long));
-    printf("sizeof(long long) = %d\n", sizeof(long long));
+    printf("sizeof(float) = %d\n", sizeof(float));
+	printf("sizeof(long float) = %d\n", sizeof(f));
+	printf("sizeof(double) = %d\n", sizeof(double));
+	printf("sizeof(long double) = %d\n", sizeof(long double));
 }
