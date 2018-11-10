423234e2dc90c4d954b7f75ff8ea357ffa5427d2
Emil Niyaz <niyaz.emil@gmail.com>
2018-11-09 20:19:57 -0500

    Return power of number

---
+++ c-functions/power-function.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int power(int, int);
+
+main () {
+	printf("%d\n", power(2, 2));
+	return 0;
+}
+
+int power(int base, int n) {
+	int i, p;
+	p = 1;
+
+	for (i = 1; i <= n; ++i) 
+		p = p * base;
+
+	return p;
+}
